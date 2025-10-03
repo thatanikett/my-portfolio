c5bc3509ae5acfa02db38bd05ef962cd0f7f4146
thatanikett <aniketsingh4269@gmail.com>
2025-10-04 03:07:00 +0530

    improved imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 // src/app/page.tsx
 import { HeroSection } from '@/components/sections/Hero'
 import { AboutSection } from '@/components/sections/About'
-import { ProjectsSection } from '@/components/sections/Projects'
+import ExpandableCardproject from '@/components/sections/Projects'
 import { ContactSection } from '@/components/sections/Contact'
 import GlobalLiquidBackground from '@/components/backgrounds/GlobalBackground'
 import SimpleNavbar from '@/components/ui/navbar'
-import ExpandableCardproject from '@/components/ui/expandable-card-grid'
-
 export default function Home() {
   return (
     <>
@@ -18,7 +16,6 @@ export default function Home() {
       <main style={{ margin: 0, padding: 0}}>
         <HeroSection />
         <AboutSection />
-        {/* <ProjectsSection /> */}
         <div style={{zIndex: 2, position: 'relative', justifyContent: 'center', alignItems: 'center', display: 'flex', padding: '2rem 1rem' }}>
             <ExpandableCardproject/>
         </div>
