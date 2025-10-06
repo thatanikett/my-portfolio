46d787cf8871fe63baedf627606c489d513f3855
thatanikett <aniketsingh4269@gmail.com>
2025-10-06 19:53:09 +0530

    improved code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,32 @@ import { HeroSection } from '@/components/sections/Hero'
 import { AboutSection } from '@/components/sections/About'
 import ExpandableCardproject from '@/components/sections/Projects'
 import { ContactSection } from '@/components/sections/Contact'
-import GlobalLiquidBackground from '@/components/backgrounds/GlobalBackground'
+import Squares from '@/components/backgrounds/Squares'
 import SimpleNavbar from '@/components/ui/navbar'
 export default function Home() {
   return (
     <>
       <SimpleNavbar />
       {/* Global background that spans entire page */}
-      <GlobalLiquidBackground />
+      <div 
+      style={{ 
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: 1,
+        overflow: 'hidden'
+      }}
+    >
+    <Squares 
+    speed={0.5} 
+    squareSize={50}
+    direction='diagonal' // up, down, left, right, diagonal
+    borderColor='#3B3347'
+    hoverFillColor='#222222'
+    />
+    </div>
       
       {/* All your content sections */}
       <main style={{ margin: 0, padding: 0}}>
